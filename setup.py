ba84acfbbf62c59e263637bbdd1af8b2e5992d33
daflongustavo <gustavodaflon@seventechnologies.cloud>
2023-12-26 16:34:21 -0300

    first release, test

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='python-phrase',
     version='1.0.0',
-    py_modules=['python-phrase'],
-    install_requires=['Click', ],
+    py_modules=['app'],
+    install_requires=['Click'],
     entry_points={
         'console_scripts': [
-            'python-phrase=python_phrase:cli',
-        ]
-    })
+            'python-phrase=app:cli',
+        ],
+    },
+)
