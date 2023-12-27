2b21a092c3f1d61ed2413062cfd5df305927b843
daflongustavo <gustavodaflon@seventechnologies.cloud>
2023-12-27 16:24:27 -0300

    testing pipeline for pypi pack publishing different versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-phrase',
-    version='1.0.1',
+    version='1.0.2',
     description='First release for this Seven Technologies Clouds API, the PythonRestCLI',
     py_modules=['app'],
     install_requires=['Click'],
